17f7a780323569f0560b027b7b711efb10326217
gutieltin <lampteyelton@gmail.com>
2026-06-22 08:34:27 +0000

    Fix welcome blade view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,10 @@
-@component('mail::message')
-# Welcome to ProjectFlow! 🎉
-
-Hi **{{ $user->name }}**,
-
-Your account has been created. You've been added to the **{{ $workspaceName }}** workspace as a **{{ ucfirst($role) }}**.
-
-Here are your login credentials:
-
-@component('mail::panel')
-**Email:** {{ $user->email }}
-
-**Temporary Password:** `{{ $tempPassword }}`
-@endcomponent
-
-@component('mail::button', ['url' => $loginUrl, 'color' => 'success'])
-Log In to ProjectFlow
-@endcomponent
-
-> ⚠️ **Important:** You will be asked to set a new password on your first login. Please keep this email safe.
-
-If you have any issues logging in, contact your manager.
-
-Thanks,
-**The ProjectFlow Team**
-@endcomponent
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ProjectFlow API</title>
+</head>
+<body>
+    <h1>ProjectFlow API</h1>
+    <p>API is running. Use /api/v1/ endpoints.</p>
+</body>
+</html>
